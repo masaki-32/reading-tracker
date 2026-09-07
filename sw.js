644ff94e833c69f0a0554535ad1753eda0bfa5dc
masaki-32 <ito24116st30@gmail.com>
2026-09-08 08:27:32 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "reading-tracker-shell-v5";
+const CACHE_NAME = "reading-tracker-shell-v6";
 const SHELL_FILES = ["./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
